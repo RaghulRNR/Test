54209496a428979f431b1996e96ab54c2d41fe3a
rahul_azure <rahulrnr173@gmail.com>
2023-01-30 11:49:08 +0530

    adding email file

---
+++ Azure_email_test.py
@@ -0,0 +1,37 @@
+
+
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
+
+## FILE TO SEND AND ITS PATH
+filename = 'report.html'
+SourcePathName  = 'D:/a/r1/a/'+filename
+print(SourcePathName)
+msg = MIMEMultipart()
+msg['From'] = 'rahulrnr173@gmail.com'
+msg['To'] = 'raghuln@nallas.com'
+msg['Subject'] = 'Tst'
+body = 'Body of the message goes in here'
+msg.attach(MIMEText(body, 'plain'))
+
+## ATTACHMENT PART OF THE CODE IS HERE
+attachment = open(SourcePathName, 'rb')
+part = MIMEBase('application', "octet-stream")
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+msg.attach(part)
+
+
+session = smtplib.SMTP('smtp.gmail.com', 587)
+session.starttls()
+session.login("rahulrnr173@gmail.com", 'dtrkwzssutnbiwmk')
+session.sendmail('rahulrnr173@gmail.com', 'rahulrnr173@gmail.com', msg.as_string())
+session.quit()
+print('Mail Sent')
+
+
